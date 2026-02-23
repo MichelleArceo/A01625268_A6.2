e3f3767a177ba3d9a0c5fca02099b7db2e6ea561
“MichelleArceo” <a01625268@tec.mx>
2026-02-23 02:39:13 +0000

    refactor: make models module re-export domain entities

---
+++ src/reservation_system/models.py
@@ -0,0 +1,7 @@
+"""Compatibility exports for domain models."""
+
+from .customer import Customer
+from .hotel import Hotel
+from .reservation import Reservation
+
+__all__ = ["Hotel", "Customer", "Reservation"]
